7c521aa4d0aea23b3fb10623910d5d21293766ff
GenkiTech <shimazu.g.r@gmail.com>
2020-01-05 16:04:46 +0900

    Fix utils.py

--- lambda/custom/utils.py
+++ lambda/custom/utils.py
@@ -1,36 +1,14 @@
 import os
 import csv
-import random
 from typing import Sequence
-from dataclasses import dataclass
 
 
-@dataclass(frozen=True)
-class Theme:
-    a: int = 0
-    b: int = 1
-
-
-def read_themes_file() -> Sequence[Sequence[str]]:
-    with open(
-            os.path.join(
-                os.getcwd(), 'Alexa笑点 - お題.csv')
-            ) as csv_file:
+def read_file(file_name: str, full_path: str = '') -> Sequence[Sequence[str]]:
+    open_file_path = full_path if full_path \
+        else os.path.join(os.getcwd(), file_name)
+    with open(open_file_path) as csv_file:
         spam_reader = csv.reader(csv_file, delimiter=',', quotechar='|')
         return [row for row in spam_reader]
-
-
-def get_row(row_num: int) -> Sequence[str]:
-    themes_table = read_themes_file()
-    return [themes[row_num] for themes in themes_table]
-
-
-def select_theme(theme: int) -> str:
-    return random.choice(get_row(theme))
-
-
-def select_themes() -> Sequence[str]:
-    return [select_theme(Theme.a), select_theme(Theme.b)]
 
 
 def get_speech_text(_, text_keys):
