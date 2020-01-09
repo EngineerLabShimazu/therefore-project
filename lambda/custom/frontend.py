73f39e06336ad3caed06db43f82411d5fb3ff90c
GenkiTech <shimazu.g.r@gmail.com>
2020-01-10 01:48:14 +0900

    Add NoHandler

--- lambda/custom/frontend.py
+++ lambda/custom/frontend.py
@@ -151,6 +151,7 @@ class ErrorHandler(AbstractExceptionHandler):
 sb = SkillBuilder()
 sb.add_request_handler(LaunchRequestHandler())
 sb.add_request_handler(YesHandler())
+sb.add_request_handler(NoHandler())
 sb.add_request_handler(HelpIntentHandler())
 sb.add_request_handler(CancelOrStopIntentHandler())
 sb.add_request_handler(SessionEndedRequestHandler())
